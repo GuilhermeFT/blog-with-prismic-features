f78f7a9dbdf490735a44b2bd4bcd63c5e7d5e49f
guilhermeft <guilhermetrind12@gmail.com>
2021-11-05 21:33:39 -0300

    feat: setup global styles and _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,37 @@
-import Document from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ReactElement } from 'react';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
